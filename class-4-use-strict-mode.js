80534d32aeca24c8e8b519e0ac1a06e6f64ef547
web-kamrul <webkamrul0001@gmail.com>
2024-01-10 11:26:41 +0600

    Spread Operatiors class 5 addded

--- class-4-use-strict-mode.js
+++ class-4-use-strict-mode.js
@@ -1,3 +1,12 @@
 // use strict JS এর Bad syntax কে Error আকারে দেখাবে যাতে ডেভোলাপারের কোডগুলো আরো সুন্দর এবং গোছানো হয়
 "use strict";
 x = 3.14;  
+
+
+x = 3.14;       // This will not cause an error.
+myFunction();
+
+function myFunction() {
+    "use strict";
+  y = 3.14;   // This will cause an error
+}
---
+++ class-5-Spread-Operator.js
@@ -0,0 +1,6 @@
+// ... কে  Spread Operator বলা হয়
+// এটি একাধিক Element কে যুক্ত করে 
+
+let poorCountry = ['Bangladesh', 'Pakistan', 'Afghanistan'];
+let richCountry = [...poorCountry, 'Canada', 'America'];
+console.log(richCountry);
