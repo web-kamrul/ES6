605bf5687ed7ffa9126d0080295f9553d0dbcc75
web-kamrul <webkamrul0001@gmail.com>
2024-01-12 23:39:22 +0600

    Class 7 new element added

--- class-7-Dynamic-Funtion.js
+++ class-7-Dynamic-Funtion.js
@@ -1,6 +1,13 @@
 // Funtion এর ভিতর যে কোনো প্যারামিটার পাস করে তাকে ডায়নামিক করা যায়।
 
-var mern5 = function (kamrul) {
-    return kamrul;
+var mern5 = function (parameter) {
+    return parameter;
 }
 console.log (mern5('Hello Wordl'));
+
+var mern6 = function (parameter) {
+    return parameter;
+}
+console.log (mern6('Kamrul Islam'));
+
+// Learn More: https://youtu.be/eISz-xBXeyI 
